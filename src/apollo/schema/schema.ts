a78e7ddbac4bd3706c9946d5ef1a53b392e02042
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-10-15 00:14:13 -0300

    feat: add mutation schema to create new user, type User and type AuthPayload

--- src/apollo/schema/schema.ts
+++ src/apollo/schema/schema.ts
@@ -2,6 +2,18 @@ import { gql } from 'apollo-server-express'
 
 export const typeDefs = gql`
 
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    user: User
+    token: String
+  }
+
   type Note {
     id: ID!
     title: String!
@@ -13,6 +25,7 @@ export const typeDefs = gql`
   }
 
   type Mutation {
+    createNewUser(name: String!, email: String!, password: String!): AuthPayload
     publishNote(title: String!, content: String!): Note
   }
 `
