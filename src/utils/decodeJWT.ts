f09e95069cee0c63df0c3ebf50bc8182d898e304
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-10-16 17:19:39 -0300

    feat: add custom scalar date to show datetime

--- src/utils/decodeJWT.ts
+++ src/utils/decodeJWT.ts
@@ -1,12 +1,11 @@
 import jwt from 'jsonwebtoken'
 
-export const decodedToken = (req: any) => {
-  const header: string = req.headers.authorization
-  if(header){
+export const decodedToken = (header: any) => {
+  try {
     const token = header.split('Bearer')[1]
     const decoded = jwt.verify(token.trimStart(), process.env.SECRET)
     return decoded
-  } else {
-    return null
+  } catch (error) {
+    return error
   }
 }
